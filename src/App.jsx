17493a32153dcabb175ea6adbcc197e04100a8c5
Casey Waweru <casey.waweru@gmail.com>
2025-10-22 23:43:56 +0300

    Initial Commit With Tailwind and React Base Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div class="border  bg-gray-800 text-black-800" >
+        Hello there
+      </div>
+  )
+}
+
+export default App
