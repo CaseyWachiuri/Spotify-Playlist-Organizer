127a723d736492d9142b7bb4570848305efe7c99
Casey Waweru <casey.waweru@gmail.com>
2025-10-31 01:27:26 +0300

    Trying it again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ["devserver-master--spotplaylistorganizer.netlify.app"],
+  },
   plugins: [
     react(),
     tailwindcss(),
